eb66ae12efa4501a929ef7a4c0412034a949cf51
Zawwar Khatri <159942012+Zawwar6@users.noreply.github.com>
2024-03-25 23:06:08 +0500

    added index.html

---
+++ app.js
@@ -0,0 +1,24 @@
+function clickMe(){
+    alert("You clicked on box1")
+}
+function clickMe2(){
+    alert("You clicked on box2")
+}
+function clickMe3(){
+    alert("You clicked on box3")
+}
+function clickMe4(){
+    alert("You clicked on box4")
+}
+function clickMe5(){
+    alert("You clicked on box5")
+}
+function clickMe6(){
+    alert("You clicked on box6")
+}
+function clickMe7(){
+    alert("You clicked on box7")
+}
+function clickMe8(){
+    alert("You clicked on box8")
+}
